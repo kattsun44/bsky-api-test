852b153a7f93eb9bde1840f6307a3c2f47e57956
kattsun44 <kattsun4416@gmail.com>
2024-05-15 23:50:36 +0900

    test: Viewing Author (my) feeds
    ref: https://docs.bsky.app/docs/tutorials/viewing-feeds#author-feeds

---
+++ viewing-my-feeds.js
@@ -0,0 +1,12 @@
+import { agent } from './login.js'
+
+const { data } = await agent.getAuthorFeed({
+  actor: process.env.DID,
+  limit: 30,
+});
+
+const { feed: postsArray, cursor: nextPage } = data;
+
+const posts = postsArray.map(e => {return [e.post.indexedAt, e.post.record.text]});
+
+console.log(posts);
