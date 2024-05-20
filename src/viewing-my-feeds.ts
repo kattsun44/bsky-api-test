ab35a8cd0f5290865600af149e83eaab216a5e83
kattsun44 <kattsun4416@gmail.com>
2024-05-21 08:04:34 +0900

    fix: viewing-{my-}feeds.js → ts

--- src/viewing-my-feeds.ts
+++ src/viewing-my-feeds.ts
@@ -1,12 +1,12 @@
 import { agent } from './login.js'
 
 const { data } = await agent.getAuthorFeed({
-  actor: process.env.DID,
+  actor: process.env.DID || "",
   limit: 30,
 });
 
 const { feed: postsArray, cursor: nextPage } = data;
 
-const posts = postsArray.map(e => {return [e.post.indexedAt, e.post.record.text]});
+const posts = postsArray.map(e => {return [e.post.indexedAt, e.post.record]});
 
 console.log(posts);
