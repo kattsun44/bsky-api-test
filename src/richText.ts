b9982633dd9c8dfe6f0b9febd9e2112ab0060218
Kattsun <kattsun4416@gmail.com>
2024-05-22 20:28:07 +0900

    Rich text test (#5)
    * add: richText.ts
      ref: https://docs.bsky.app/docs/tutorials/creating-a-post#mentions-and-links
    * update: set facets manually

---
+++ src/richText.ts
@@ -0,0 +1,49 @@
+import { RichText } from '@atproto/api'
+import { agent } from './login.js'
+
+import { AppBskyRichtextFacet } from '@atproto/api'
+
+type Facet = AppBskyRichtextFacet.Main
+
+const rt = new RichText({
+  text: 'This is RichText Test by @kattsun.dev. My Github URL is https://github.com/kattsun44.'
+})
+await rt.detectFacets(agent)
+
+console.log('rt.text: ', rt.text)
+console.log('rt.facets:')
+console.log('%o', rt.facets)
+
+// await agent.post({
+//   text: rt.text,
+//   facets: rt.facets,
+//   createdAt: new Date().toISOString()
+// })
+
+const rt2 = new RichText({
+  text: 'Hello, World! This is RichText Test by @kattsun.dev.',
+  facets: [{
+    $type: 'app.bsky.richtext.facet',
+    index: {
+      byteStart: 0,
+      byteEnd: 5,
+    },
+    features: [
+      {
+        $type: 'app.bsky.richtext.facet#link',
+        uri: 'https://github.com/kattsun44'
+      }
+    ]
+  }]
+})
+
+console.log('rt2.text: ', rt2.text)
+console.log('rt2.facets:')
+console.log('%o', rt2.facets)
+
+// await agent.post({
+//   text: rt2.text,
+//   facets: rt2.facets,
+//   createdAt: new Date().toISOString()
+// })
+
